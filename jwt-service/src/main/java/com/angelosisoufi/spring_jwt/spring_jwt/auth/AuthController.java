07a0f1ef2dff14fbdd2dbec9d8deea27caf0be66
aggelosisoufi <angelosisoufi@gmail.com>
2026-02-15 00:18:17 +0200

    Replace field DI and use @Service annotation instead of @Component one

--- jwt-service/src/main/java/com/angelosisoufi/spring_jwt/spring_jwt/auth/AuthController.java
+++ jwt-service/src/main/java/com/angelosisoufi/spring_jwt/spring_jwt/auth/AuthController.java
@@ -4,7 +4,6 @@ import com.angelosisoufi.spring_jwt.spring_jwt.model.SignUpRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,15 +11,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    private final AuthService service;
 
-    @Autowired
-    AuthService service;
+    public AuthController(AuthService service) {
+        this.service = service;
+    }
 
     @PostMapping(value = "/signin", consumes = "application/json", produces = "application/json")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody AuthenticationRequest request, HttpServletResponse response) {
@@ -36,7 +36,7 @@ public class AuthController {
     }
 
     @PostMapping(value = "/refresh-token", produces = "application/json")
-    public ResponseEntity<?> refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public ResponseEntity<?> refreshToken(HttpServletRequest request, HttpServletResponse response) {
         service.refreshToken(request, response);
         return ResponseEntity.ok(Map.of("message", "Access token refreshed successfully"));
     }
--- jwt-service/src/main/java/com/angelosisoufi/spring_jwt/spring_jwt/security/JwtService.java
+++ jwt-service/src/main/java/com/angelosisoufi/spring_jwt/spring_jwt/security/JwtService.java
@@ -8,13 +8,13 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.*;
 import java.util.function.Function;
 
-@Component
+@Service
 public class JwtService {
     @Value("${jwt.secret}")
     private String jwtSecret;
